1ac6d263be91a561488385bf143f10fce70d748a
Lorenzo Donini <lorenzo.donini@tum.de>
2016-08-22 19:00:44 +0200

    Edit priority queue implementation
    Add root struct
    Edit node struct by adding a pointer to previous element
    Edit implementations of push, pop, init, deinit accordingly
    Add peek function
    Add computeBufferSize function

--- core/queue.c
+++ core/queue.c
@@ -13,80 +13,125 @@
 
 
 
-qnode_t * init_queue ()
-{
-	qnode_t* q = (qnode_t*) calloc (1, sizeof(qnode_t));
-	q->q_mutex = PTHREAD_MUTEX_INITIALIZER;
+void * init_queue () {
+	qhead_t* q = (qhead_t*) calloc(1, sizeof(qhead_t));
+	if (pthread_mutex_init(&q->q_mutex, NULL) != 0)
+	{
+		printf("\n mutex init failed\n");
+		return NULL;
+	}
 	return q;
 }
 
+void push (qhead_t* root, S3TP_PACKET* data) {
+	qnode_t *ref, *newNode, *swap;
 
-void push (
-		qnode_t* head,
-		S3TP_PACKET* data)
-{
-	int i;
-	qnode_t *ref = head;
-	qnode_t * new, tmp;
+	//Enter critical section
+	pthread_mutex_lock(&root->q_mutex);
 
-	pthread_mutex_lock(&head->q_mutex);
-
-	while(1)
-	{
-		if(ref->next ==0)
-		{
-			//either we are at the last node;
-			break;
-		}else if (ref->next->seq > data->hdr->seq)
-		{
-			break;
-			//or our seq number ist lower than the next one, reordering
-
-		}
-		ref = ref->next;
+	if (root->size >= MAX_QUEUE_CAPACITY) {
+		//Queue is full, dropping new packet
+		printf("Queue is full, dropping packet with sequence number %d\n", data->hdr.seq);
+		//Exit critical section
+		pthread_mutex_unlock(&root->q_mutex);
+		return;
 	}
 
-	//create a new node
-	new = (qnode_t*) calloc(1, sizeof(qnode_t));
-	new->seq = data->hdr->seq;
+	//Creating new node
+	newNode = (qnode_t*) calloc(1, sizeof(qnode_t));
+	newNode->seq = data->hdr.seq;
+	newNode->payload = data;
 
-	//insert here
-	tmp = ref->next;
-	ref->next = new;
-	new->next = tmp;
+	//Inserting new node inside the priority queue
+	ref = root->tail;
+	while (1) {
+		if (ref == NULL) {
+			//We are at the head of the queue. This is due to the queue being empty.
+			root->head = newNode;
+			root->tail = newNode;
+			break;
+		}
+		else if (ref->seq < data->hdr.seq) {
+			//New node has higher sequence number than current element. New node has lower priority -> append it here
+			swap = ref->next;
+			ref->next = newNode;
+			newNode->prev = ref;
+			newNode->next = swap;
+			if (swap != NULL) {
+				swap->prev = newNode;
+			} else {
+				root->tail = newNode;
+			}
+			break;
+		} else if (ref->prev == NULL) {
+			//We are at the head of the queue
+			ref->prev = newNode;
+			newNode->next = ref;
+			root->head = newNode;
+			break;
+		}
+		//Current sequence number is higher than this one. New node has higher priority
+		ref = ref->prev;
+	}
 
-	pthread_mutex_unlock(&head->q_mutex);
+	//Increase current buffer size
+	root->size += 1;
 
+	//Exit critical section
+	pthread_mutex_unlock(&root->q_mutex);
 }
 
-S3TP_PACKET* pop (
-		qnode_t* head)
-{
-	qnode_t* tmp;
+S3TP_PACKET* peek (qhead_t* root) {
+	qnode_t * head = root->head;
+	//Returning payload if head is not null, otherwise return null
+	return (head != NULL) ? head->payload : NULL;
+}
+
+S3TP_PACKET* pop (qhead_t* root) {
+	qnode_t* ref;
 	S3TP_PACKET* pack;
 
-	pthread_mutex_lock(&head->q_mutex);
+	//Entering critical section
+	pthread_mutex_lock(&root->q_mutex);
 
 	//get the lowest seq packet and remove it from queue
-	tmp = head->next;
-	head->next = head->next->next;
-	pack = tmp->payload;
+	ref = root->head;
+	if (ref == NULL) {
+		return NULL;
+	}
 
-	free(tmp);
+	if (ref->next == NULL) {
+		//This is the only element in the queue
+		root->tail = NULL;
+		root->head = NULL;
+	} else {
+		root->head = ref->next;
+		ref->next->prev = NULL;
+	}
 
-	pthread_mutex_unlock(&head->q_mutex);
+	pack = ref->payload;
+	free(ref);
+
+	//Decrease current buffer size
+	root->size -= 1;
+
+	//Exiting critical section
+	pthread_mutex_unlock(&root->q_mutex);
 
 	return pack;
 }
 
-void deinit_queue(
-		qnode_t* head)
+void deinit_queue(qhead_t* root)
 {
-	qnode_t* ref;
-	while(head!=0)
-	{
-		ref = head;
-		head = head->next;
+	qnode_t* ref = root->head;
+	while (ref != NULL) {
+		root->head = ref->next;
 		free(ref);
+		ref = root->head;
 	}
+	free(root);
+}
+
+u32 computeBufferSize(qhead_t* root) {
+	return root->size * sizeof(S3TP_PACKET);
 }
--- core/queue.h
+++ core/queue.h
@@ -13,19 +13,31 @@
 #include "s3tp_types.h"
 #include <pthread.h>
 
+#define MB 1 << 20
+#define MAX_QUEUE_CAPACITY (1*MB) / sizeof(S3TP_PACKET)
+
+typedef struct tag_mini_queue_root qhead_t;
 typedef struct tag_mini_queue_node qnode_t;
 
-struct tag_mini_queue_node{
-	S3TP_PACKET* payload;
+struct tag_mini_queue_root {
+	qnode_t * head;
+	qnode_t * tail;
 	pthread_mutex_t q_mutex;
+	u16 size;
+};
+
+struct tag_mini_queue_node {
+	S3TP_PACKET* payload;
 	qnode_t * next;
+	qnode_t * prev;
 	int seq;
 };
 
-
 void* init_queue ();
-void push (qnode_t *head, S3TP_PACKET* data);
-S3TP_PACKET* pop ();
-void deinit_queue(qnode_t* head);
+void push (qhead_t *root, S3TP_PACKET* data);
+S3TP_PACKET* pop (qhead_t* root);
+S3TP_PACKET* peek (qhead_t* root);
+void deinit_queue (qhead_t* root);
+u32 computeBufferSize (qhead_t* root);
 
 #endif /* CORE_QUEUE_H_ */
