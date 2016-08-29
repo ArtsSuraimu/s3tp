811b15a0cc9ea02a03252a5b33edb1f12a56661d
Lorenzo Donini <lorenzo.donini@tum.de>
2016-08-29 13:51:31 +0200

    Update SimpleQueue

--- core/SimpleQueue.h
+++ core/SimpleQueue.h
@@ -10,6 +10,8 @@
 #include <assert.h>
 
 #define SIMPLE_QUEUE_DEFAULT_CAPACITY 256
+#define SIMPLE_QUEUE_BLOCKING 1
+#define SIMPLE_QUEUE_NON_BLOCKING 0
 #define SIMPLE_QUEUE_FULL -1
 #define SIMPLE_QUEUE_PUSH_SUCCESS 0
 
@@ -41,6 +43,9 @@ struct tag_s3tp_raw_data {
     }
 };
 
+/*
+ * Simple Queue
+ */
 template <typename T>
 struct tag_s3tp_simple_queue_node {
     T element;
@@ -50,7 +55,7 @@ struct tag_s3tp_simple_queue_node {
 template <typename T>
 struct SimpleQueue {
 public:
-    SimpleQueue(int capacity);
+    SimpleQueue(int capacity, int blocking);
     ~SimpleQueue();
     int push(const T& element);
     bool isEmpty();
@@ -61,7 +66,10 @@ private:
     tag_s3tp_simple_queue_node<T> * tail;
     int size;
     int capacity;
+    int mode;
     pthread_mutex_t q_mutex;
+    pthread_cond_t content_add_cond;
+    pthread_cond_t content_rmv_cond;
 };
 
 
@@ -70,12 +78,15 @@ private:
  */
 
 template <typename T>
-SimpleQueue<T>::SimpleQueue(int capacity) {
+SimpleQueue<T>::SimpleQueue(int capacity, int mode) {
     head = NULL;
     tail = NULL;
     this->capacity = (capacity > 0) ? capacity : SIMPLE_QUEUE_DEFAULT_CAPACITY;
     size = 0;
+    this->mode = mode;
     pthread_mutex_init(&q_mutex, NULL);
+    pthread_cond_init(&content_add_cond, NULL);
+    pthread_cond_init(&content_rmv_cond, NULL);
 }
 
 template <typename T>
@@ -94,8 +105,14 @@ template <typename T>
 int SimpleQueue<T>::push(const T &element) {
     pthread_mutex_lock(&q_mutex);
     if (size == capacity) {
-        pthread_mutex_unlock(&q_mutex);
-        return SIMPLE_QUEUE_FULL;
+        if (mode == SIMPLE_QUEUE_BLOCKING) {
+            //Wait for queue to free some space
+            pthread_cond_wait(&content_rmv_cond, &q_mutex);
+        } else {
+            //Don't wait, just return error code
+            pthread_mutex_unlock(&q_mutex);
+            return SIMPLE_QUEUE_FULL;
+        }
     }
     tag_s3tp_simple_queue_node<T> * node = new tag_s3tp_simple_queue_node<T>();
     node->element = element;
@@ -107,24 +124,35 @@ int SimpleQueue<T>::push(const T &element) {
     tail = node;
     size++;
 
+    pthread_cond_signal(&content_add_cond);
     pthread_mutex_unlock(&q_mutex);
     return SIMPLE_QUEUE_PUSH_SUCCESS;
 }
 
 template <typename T>
 bool SimpleQueue<T>::isEmpty() {
-    return size == 0;
+    pthread_mutex_lock(&q_mutex);
+    bool result = size == 0;
+    pthread_mutex_unlock(&q_mutex);
+    return result;
 }
 
 template <typename T>
 T& SimpleQueue<T>::pop() {
-    assert(head != NULL);
+    pthread_mutex_lock(&q_mutex);
+    if (mode == SIMPLE_QUEUE_BLOCKING) {
+        pthread_cond_wait(&content_add_cond, &q_mutex);
+    } else {
+        assert(head != NULL);
+    }
 
     tag_s3tp_simple_queue_node<T> * node = head;
     head = node->next;
     T& result = node->element;
     delete node;
     size--;
+    pthread_cond_signal(&content_rmv_cond);
+    pthread_mutex_unlock(&q_mutex);
     return result;
 }
 
