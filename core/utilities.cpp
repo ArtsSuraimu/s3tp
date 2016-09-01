230113da6f71c597951f31e5adbb009d57d18be0
Lorenzo Donini <lorenzo.donini@tum.de>
2016-09-01 19:26:37 +0200

    Remove dead code

--- core/utilities.cpp
+++ core/utilities.cpp
@@ -13,40 +13,6 @@
 
 #define CRC16 0x8005
 
-/*u16 gen_crc16(const u8 *data, u16 size)
-{
-	u16 out = 0;
-    int bits_read = 0, bit_flag;
-
-    /* Sanity check:
-    if(data == NULL)
-        return 0;
-
-    while(size > 0)
-    {
-        bit_flag = out >> 15;
-
-        /* Get next bit:
-        out <<= 1;
-        out |= (*data >> (7 - bits_read)) & 1;
-
-        /* Increment bit counter:
-        bits_read++;
-        if(bits_read > 7)
-        {
-            bits_read = 0;
-            data++;
-            size--;
-        }
-
-        /* Cycle check:
-        if(bit_flag)
-            out ^= CRC16;
-
-    }
-    return out;
-}*/
-
 uint16_t calc_checksum(const char *data, uint16_t size)
 {
 	uint16_t out = 0;
