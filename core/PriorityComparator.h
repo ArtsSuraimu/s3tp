2c49673adeb5c5996fea7b8a4dc8076063d5c90a
Lorenzo Donini <lorenzo.donini@tum.de>
2016-09-07 00:50:14 +0200

    Add comparator interface for priority queue ordering

---
+++ core/PriorityComparator.h
@@ -0,0 +1,14 @@
+//
+// Created by Lorenzo Donini on 07/09/16.
+//
+
+#ifndef S3TP_PRIORITYCOMPARATOR_H
+#define S3TP_PRIORITYCOMPARATOR_H
+
+template <typename T>
+class PriorityComparator {
+public:
+    virtual int comparePriority(T element1, T element2) = 0;
+};
+
+#endif //S3TP_PRIORITYCOMPARATOR_H
