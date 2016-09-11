762a15a0c01c86fd1f5a5e28d4aa64bb531af454
lorenzodonini <lorenzo.donini90@gmail.com>
2016-09-11 23:46:37 +0200

    Adapt Buffer to new s3tp packet structure

--- core/Buffer.cpp
+++ core/Buffer.cpp
@@ -5,7 +5,7 @@
 #include "Buffer.h"
 
 //Ctor
-Buffer::Buffer(PriorityComparator<S3TP_PACKET_WRAPPER*> * comparator) {
+Buffer::Buffer(PriorityComparator<S3TP_PACKET*> * comparator) {
     pthread_mutex_init(&buffer_mutex, NULL);
     this->comparator = comparator;
 }
@@ -13,7 +13,7 @@ Buffer::Buffer(PriorityComparator<S3TP_PACKET_WRAPPER*> * comparator) {
 //Dtor
 Buffer::~Buffer() {
     pthread_mutex_lock(&buffer_mutex);
-    for (std::map<int, PriorityQueue<S3TP_PACKET_WRAPPER*> *>::iterator it = queues.begin(); it != queues.end(); ++it) {
+    for (std::map<int, PriorityQueue<S3TP_PACKET*> *>::iterator it = queues.begin(); it != queues.end(); ++it) {
         delete it->second;
     }
     queues.clear();
@@ -30,26 +30,27 @@ bool Buffer::packetsAvailable() {
     return result;
 }
 
-int Buffer::write(S3TP_PACKET_WRAPPER * packet) {
+int Buffer::write(S3TP_PACKET * packet) {
     pthread_mutex_lock(&buffer_mutex);
-    int port = packet->pkt->hdr.getPort();
+    S3TP_HEADER * hdr = packet->getHeader();
+    int port = hdr->getPort();
 
-    PriorityQueue<S3TP_PACKET_WRAPPER*> * queue = queues[port];
+    PriorityQueue<S3TP_PACKET*> * queue = queues[port];
     if (queue == NULL) {
         //Adding new queue to the internal map
-        queue = new PriorityQueue<S3TP_PACKET_WRAPPER*>();
+        queue = new PriorityQueue<S3TP_PACKET*>();
         queues[port] = queue;
     }
     if (queue->push(packet, comparator) == QUEUE_FULL) {
         LOG_INFO(std::string("Queue " + std::to_string(port)
                               + " full. Dropped packet with sequence number "
-                              + std::to_string(packet->pkt->hdr.seq_port)));
+                              + std::to_string((int)hdr->seq_port)));
     } else {
         packet_counter[port] = queue->getSize();
         LOG_DEBUG(std::string("Queue " + std::to_string(port)
                               + ": packet "
-                              + std::to_string(packet->pkt->hdr.seq_port) + " written ("
-                              + std::to_string(packet->pkt->hdr.getPduLength()) + " bytes)"));
+                              + std::to_string((int)hdr->seq_port) + " written ("
+                              + std::to_string((int)hdr->getPduLength()) + " bytes)"));
     }
 
     pthread_mutex_unlock(&buffer_mutex);
@@ -57,38 +58,38 @@ int Buffer::write(S3TP_PACKET_WRAPPER * packet) {
     return CODE_SUCCESS;
 }
 
-PriorityQueue<S3TP_PACKET_WRAPPER *> * Buffer::getQueue(int port) {
+PriorityQueue<S3TP_PACKET *> * Buffer::getQueue(int port) {
     pthread_mutex_lock(&buffer_mutex);
-    PriorityQueue<S3TP_PACKET_WRAPPER*> * queue = queues[port];
+    PriorityQueue<S3TP_PACKET*> * queue = queues[port];
     pthread_mutex_unlock(&buffer_mutex);
     return queue;
 }
 
-S3TP_PACKET_WRAPPER * Buffer::getNextPacket(int port) {
+S3TP_PACKET * Buffer::getNextPacket(int port) {
     pthread_mutex_lock(&buffer_mutex);
-    S3TP_PACKET_WRAPPER * packet = popPacketInternal(port);
+    S3TP_PACKET * packet = popPacketInternal(port);
     pthread_mutex_unlock(&buffer_mutex);
     return packet;
 }
 
-S3TP_PACKET_WRAPPER * Buffer::getNextAvailablePacket() {
+S3TP_PACKET * Buffer::getNextAvailablePacket() {
     pthread_mutex_lock(&buffer_mutex);
     if (packet_counter.empty()) {
         pthread_mutex_unlock(&buffer_mutex);
         return NULL;
     }
     std::map<int, int>::iterator it = packet_counter.begin();
-    S3TP_PACKET_WRAPPER * packet = popPacketInternal(it->first);
+    S3TP_PACKET * packet = popPacketInternal(it->first);
     pthread_mutex_unlock(&buffer_mutex);
     return packet;
 }
 
-S3TP_PACKET_WRAPPER * Buffer::popPacketInternal(int port) {
-    PriorityQueue<S3TP_PACKET_WRAPPER*> * queue = queues[port];
+S3TP_PACKET * Buffer::popPacketInternal(int port) {
+    PriorityQueue<S3TP_PACKET*> * queue = queues[port];
     if (queue == NULL || queue->isEmpty()) {
         return NULL;
     }
-    S3TP_PACKET_WRAPPER * packet = queue->pop();
+    S3TP_PACKET * packet = queue->pop();
     if (queue->isEmpty()) {
         packet_counter.erase(port);
     } else {
--- core/Buffer.h
+++ core/Buffer.h
@@ -10,22 +10,22 @@
 
 class Buffer {
 public:
-    Buffer(PriorityComparator<S3TP_PACKET_WRAPPER*> * comparator);
+    Buffer(PriorityComparator<S3TP_PACKET*> * comparator);
     ~Buffer();
     bool packetsAvailable();
-    int write(S3TP_PACKET_WRAPPER * packet);
-    PriorityQueue<S3TP_PACKET_WRAPPER *> * getQueue(int port);
-    S3TP_PACKET_WRAPPER * getNextPacket(int port);
-    S3TP_PACKET_WRAPPER * getNextAvailablePacket();
+    int write(S3TP_PACKET * packet);
+    PriorityQueue<S3TP_PACKET *> * getQueue(int port);
+    S3TP_PACKET * getNextPacket(int port);
+    S3TP_PACKET * getNextAvailablePacket();
 
 private:
-    PriorityComparator<S3TP_PACKET_WRAPPER *> * comparator;
-    std::map<int, PriorityQueue<S3TP_PACKET_WRAPPER*>*> queues;
+    PriorityComparator<S3TP_PACKET *> * comparator;
+    std::map<int, PriorityQueue<S3TP_PACKET*>*> queues;
     std::map<int, int> packet_counter;
 
     pthread_mutex_t buffer_mutex;
 
-    S3TP_PACKET_WRAPPER * popPacketInternal(int port);
+    S3TP_PACKET * popPacketInternal(int port);
 };
 
 #endif //S3TP_BUFFER_H
