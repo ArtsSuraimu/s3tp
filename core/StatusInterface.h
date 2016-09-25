32fd610296c9856ec35352cb0d6774f857d8224f
lorenzodonini <lorenzo.donini90@gmail.com>
2016-09-25 17:35:00 +0200

    Add callback method to StatusInterface for q status

--- core/StatusInterface.h
+++ core/StatusInterface.h
@@ -11,6 +11,7 @@ public:
     virtual void onChannelStatusChanged(uint8_t channel, bool active) = 0;
     virtual void onError(int error, void * params) = 0;
     virtual void onSynchronization(uint8_t syncId) = 0;
+    virtual void onOutputQueueAvailable(uint8_t port) = 0;
 };
 
 #endif //S3TP_LINKSTATUSINTERFACE_H
