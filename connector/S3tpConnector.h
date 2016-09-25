aeb35fd97f080497e8592e2af8840b0541ab51a7
lorenzodonini <lorenzo.donini90@gmail.com>
2016-09-25 13:56:03 +0200

    Add doc for connector send method

--- connector/S3tpConnector.h
+++ connector/S3tpConnector.h
@@ -22,6 +22,14 @@ public:
     S3tpConnector();
     ~S3tpConnector();
     int init(S3TP_CONFIG config, S3tpCallback * callback);
+    /**
+     * Sends data to the underlying transport layer (S3TP).
+     * As the S3TP module could have its output buffer full and temporarily not be able to send more data,
+     * this method will block as long as long as the data was not written entirely.
+     * @param data  Pointer to the data to be written. Can be any kind of data, as long as it is contiguous in memory.
+     * @param len  The amount of data to be written (i.e. the length of the passed structure).
+     * @return  Returns the number of bytes sent.
+     */
     int send(const void * data, size_t len);
     char * recvRaw(size_t * len, int * error);
     int recv(void * buffer, size_t len);
