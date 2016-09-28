8c3a16dd1e9c6e550beaaa3317027d32e8751385
lorenzodonini <lorenzo.donini90@gmail.com>
2016-09-28 11:13:23 +0200

    Add maximumWindowReached method to PolicyActor

--- core/PolicyActor.h
+++ core/PolicyActor.h
@@ -10,6 +10,7 @@ class PolicyActor {
 public:
     virtual int comparePriority(T element1, T element2) = 0;
     virtual bool isElementValid(T element) = 0;
+    virtual bool maximumWindowExceeded(T queueHead, T newElement) = 0;
 };
 
 #endif //S3TP_PRIORITYCOMPARATOR_H
