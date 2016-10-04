65f85da2e352b01a7138f7ff115cf585cc553e2f
lorenzodonini <lorenzo.donini90@gmail.com>
2016-10-04 15:06:36 +0200

    move onSynchronization to transportInterface

--- core/StatusInterface.h
+++ core/StatusInterface.h
@@ -10,7 +10,6 @@ public:
     virtual void onLinkStatusChanged(bool active) = 0;
     virtual void onChannelStatusChanged(uint8_t channel, bool active) = 0;
     virtual void onError(int error, void * params) = 0;
-    virtual void onSynchronization(uint8_t syncId) = 0;
     virtual void onOutputQueueAvailable(uint8_t port) = 0;
 };
 
