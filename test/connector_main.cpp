3b07e5cc1e4d258ebb3ab079faffab78031fd103
lorenzodonini <lorenzo.donini90@gmail.com>
2016-10-11 12:30:58 +0200

    update connector main with listen/connect modes

--- test/connector_main.cpp
+++ test/connector_main.cpp
@@ -6,6 +6,9 @@
 #include "../connector/S3tpConnector.h"
 #include <csignal>
 
+std::condition_variable newConnectionCond;
+std::mutex mainMutex;
+
 /*
  * Main Test Program
  */
@@ -21,10 +24,37 @@ class CallbackClass: public S3tpCallback {
     virtual void onError(int code, char * error) {
         LOG_ERROR(std::string("Received error with code " + std::to_string(code)));
     }
+
+    virtual void onConnectionUp() {
+        LOG_INFO(std::string("Connection was established"));
+        newConnectionCond.notify_all();
+    }
+
+    virtual void onConnectionDown(int code) {
+        LOG_INFO(std::string("Connection was closed with code " + std::to_string(code)));
+    }
 };
 
+void communicationRoutine(S3tpConnector& connector) {
+    std::string data = "";
+
+    while (connector.isBound()) {
+        std::cout << "Please insert a message to send: \n";
+        std::getline(std::cin, data);
+        if (data.compare("") == 0) {
+            break;
+        }
+        connector.send((void *)data.data(), data.length());
+    }
+
+    connector.closeConnection();
+
+    std::cout << "Lost connection to s3tpd. Exiting" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
-    if (argc != 2)
+    //args: socket_path L/C
+    if (argc != 3)
     {
         std::cout << "Illegal arguments\n";
         return -1;
@@ -32,7 +62,8 @@ int main(int argc, char* argv[]) {
 
     signal(SIGPIPE, SIG_IGN);
 
-    std::string testS = "";
+    std::string appPort = "";
+    std::string mode;
     S3tpConnector connector;
     S3TP_CONFIG config;
     CallbackClass callback;
@@ -41,30 +72,33 @@ int main(int argc, char* argv[]) {
     int argi = 1;
 
     socket_path = argv[argi++];
-
-    std::cout << "Please select a port to connect to: ";
-    std::getline(std::cin, testS);
-    port = std::stoi(testS);
+    mode = argv[argi++];
+    std::cout << "Please select the application port: ";
+    std::getline(std::cin, appPort);
+    port = std::stoi(appPort);
     config.port = (uint8_t)port;
     config.channel = 3;
     config.options = S3TP_OPTION_ARQ;
-
     connector.init(config, &callback);
     sleep(1);
 
-    int i=0;
-    while (connector.isConnected()) {
-        std::cout << "Please insert a message to send: \n";
-        std::getline(std::cin, testS);
-        if (testS.compare("") == 0) {
-            break;
+    if (mode.compare("L") == 0) {
+        std::cout << "Listening for incoming connections..." << std::endl;
+        connector.listen();
+        std::unique_lock<std::mutex> lock(mainMutex);
+        newConnectionCond.wait(lock);
+        std::cout << "Connected to remote host!" << std::endl;
+        communicationRoutine(connector);
+    } else if (mode.compare("C") == 0) {
+        if (connector.connectToRemoteHost()) {
+            std::cout << "Connected to remote host!" << std::endl;
+            communicationRoutine(connector);
+        } else {
+            std::cout << "Connection to remote host failed. Quitting" << std::endl;
         }
-        connector.send((void *)testS.data(), testS.length());
+    } else {
+        std::cout << "Invalid mode. Quitting" << std::endl;
     }
-
-    connector.closeConnection();
-
-    std::cout << "Lost connection to s3tpd. Exiting" << std::endl;
 
     return 0;
 }
