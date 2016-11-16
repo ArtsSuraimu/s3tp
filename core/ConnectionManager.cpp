b56c24bcf3467db28c5292ebf452a25a835388fd
lorenzodonini <lorenzo.donini90@gmail.com>
2016-11-16 12:32:23 +0100

    add connection manager class

---
+++ core/ConnectionManager.cpp
@@ -0,0 +1,70 @@
+//
+// Created by lorenzodonini on 13.11.16.
+//
+
+#include "ConnectionManager.h"
+
+std::shared_ptr<Connection> ConnectionManager::getConnection(uint8_t localPort) {
+    connectionsMutex.lock();
+    std::shared_ptr<Connection> connection = openConnections[localPort];
+    connectionsMutex.unlock();
+
+    return connection;
+}
+
+bool ConnectionManager::handleNewConnection(S3TP_PACKET * newConnectionRequest) {
+    connectionsMutex.lock();
+
+    if (openConnections[newConnectionRequest->getHeader()->srcPort]) {
+        //Port is open already
+        connectionsMutex.unlock();
+        return false;
+    }
+    std::shared_ptr<Connection> connection(new Connection(newConnectionRequest));
+    openConnections[newConnectionRequest->getHeader()->srcPort];
+    connectionsMutex.unlock();
+
+    return true;
+}
+
+bool ConnectionManager::openConnection(uint8_t srcPort, uint8_t destPort, uint8_t virtualChannel, uint8_t options) {
+    connectionsMutex.lock();
+    if (openConnections[srcPort]) {
+        //Port is open already
+        connectionsMutex.unlock();
+        return false;
+    }
+    std::shared_ptr<Connection> connection(new Connection(srcPort, destPort, virtualChannel, options));
+    openConnections[srcPort] = connection;
+    connectionsMutex.unlock();
+
+    return true;
+}
+
+bool ConnectionManager::closeConnection(uint8_t srcPort) {
+    connectionsMutex.lock();
+    std::shared_ptr<Connection> connection = openConnections[srcPort];
+    if (connection == nullptr) {
+        //Port was closed to begin with
+        connectionsMutex.unlock();
+        return false;
+    }
+    Connection::STATE state = (*connection).getCurrentState();
+    if (state == Connection::DISCONNECTING || state == Connection::DISCONNECTED) {
+        //Cannot close port right now
+        connectionsMutex.unlock();
+        return false;
+    }
+    (*connection).close();
+    connectionsMutex.unlock();
+
+    return true;
+}
+
+int ConnectionManager::openConnectionsCount() {
+    connectionsMutex.lock();
+    int openConnectionsCount = (int)openConnections.size();
+    connectionsMutex.unlock();
+
+    return openConnectionsCount;
+}
---
+++ core/ConnectionManager.h
@@ -0,0 +1,28 @@
+//
+// Created by lorenzodonini on 13.11.16.
+//
+
+#ifndef S3TP_CONNECTIONMANGER_H
+#define S3TP_CONNECTIONMANGER_H
+
+#include "Connection.h"
+#include "ConnectionStatusInterface.h"
+#include <map>
+
+class ConnectionManager {
+private:
+    std::map<uint8_t, std::shared_ptr<Connection>> openConnections;
+    std::mutex connectionsMutex;
+
+public:
+    ~ConnectionManager() {};
+
+    std::shared_ptr<Connection> getConnection(uint8_t localPort);
+    bool handleNewConnection(S3TP_PACKET * newConnectionRequest);
+    bool openConnection(uint8_t srcPort, uint8_t destPort, uint8_t virtualChannel, uint8_t options);
+    bool closeConnection(uint8_t srcPort);
+    int openConnectionsCount();
+};
+
+
+#endif //S3TP_CONNECTIONMANGER_H
